085d6deb9c4190a37c1d301b8316414bfcf6dd5c
AmurdAmzer <amurdamzer@gmail.com>
2025-10-14 10:09:24 -0400

    Group Anagram update

--- '''Group Anagrams.py
+++ '''Group Anagrams.py
@@ -24,3 +24,20 @@ Constraints:
 1 <= strs.length <= 1000.
 0 <= strs[i].length <= 100
 strs[i] is made up of lowercase English letters. '''
+
+# using sorting Time O(n * k log k); space O(n)
+# step 1: create a diffult dictionary and assign it to groups (groups of words), where the key is the sorted word/string and the value is the list of words that are the same or anagrams
+         from collections import defaultdict
+
+         groups = defaultdict(list)
+
+         # step 2: sort the words and save the sorted words to keys, then append/add anagrams to their keys
+
+         for word in strs:
+            key = "".join(sorted(word))
+            groups[key].append(word)
+
+        
+        # step 3: return the list of anagrams
+
+         return list(groups.values())
